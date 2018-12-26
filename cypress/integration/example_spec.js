b8ef31cfef6d1a9f140418490ac0d10d145a03f5
shas998 <shashikantrai998@yahoo.com>
2018-12-26 18:20:51 +0530

    update

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -11,10 +11,10 @@
 // Please read our "Introduction to Cypress"
 // https://on.cypress.io/introduction-to-cypress
 
-describe('Kitchen Sink', function () {
+describe('First Scenario', function () {
   it('.should() - assert that <title> is correct', function () {
     // https://on.cypress.io/visit
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://www.t-mobile.cz/tarify')
 
     // Here we've made our first assertion using a '.should()' command.
     // An assertion is comprised of a chainer, subject, and optional value.
@@ -23,7 +23,7 @@ describe('Kitchen Sink', function () {
     // https://on.cypress.io/and
 
     // https://on.cypress.io/title
-    cy.title().should('include', 'Kitchen Sink')
+    cy.title().should('include', 'Neomezené tarify: Volání, SMS a internet v mobilu - T-Mobile.cz')
     //   ↲               ↲            ↲
     // subject        chainer      value
   })
